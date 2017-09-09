48d99988eb3609ef3b90ec1b1a5c130569cec11c
michalkmet <michalkmet6@gmail.com>
2017-09-09 12:49:20 +0200

    add theme support

--- functions.php
+++ functions.php
@@ -22,6 +22,15 @@
  */
 
 
+/**
+ * Add theme support by Michal
+ */
+
+add_theme_support( 'post-thumbnails' );
+add_theme_support( 'menus' );
+add_theme_support( 'custom-background' );
+add_theme_support( 'custom-header' );
+add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
 
 
 
